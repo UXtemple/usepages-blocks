454d8b5f5962b162fd763e30b1b6a247d4ded795
Darío Javier Cravero <dario@uxtemple.com>
2016-02-09 13:26:35 +0000

    feat: show unknown block better

--- unknown.js
+++ unknown.js
@@ -1,3 +1,31 @@
+import Code from './code';
 import React from 'react';
+import stringify from 'json-stable-stringify';
 
-export default props => <div>{JSON.stringify(props)}</div>;
+export default props => {
+  const { _block, ...rest } = props;
+
+  const code = stringify({
+    block: _block,
+    props: rest
+  }, {space: '\t'});
+
+  return (
+    <div style={style.wrapper}>
+      <div>Sorry but I don't know how to render this block :/</div>
+      <Code code={code} gutter={false} readOnly={true} highlightActiveLine={false} style={style.code} />
+    </div>
+  );
+}
+
+const style = {
+  code: {
+    marginTop: 10
+  },
+  wrapper: {
+    backgroundColor: '#323232',
+    color: 'white',
+    fontSize: 12,
+    padding: 20
+  }
+};
