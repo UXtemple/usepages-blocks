97c0248801b9e940bf89d826f5080b89b1159ce8
Darío Javier Cravero <dario@uxtemple.com>
2016-04-11 00:17:01 +0100

    fix: don't trip if style isn't set in Knocking

--- knocking.js
+++ knocking.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Waiting from 'waiting';
 
-const Knocking = props => (
-  <div style={props.style}>
-    <Waiting color={props.style.color} size={props.size} />
+const Knocking = ({style={}, size}) => (
+  <div style={style}>
+    <Waiting color={style.color} size={size} />
   </div>
 );
 export default Knocking;
