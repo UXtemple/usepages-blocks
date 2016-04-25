45f377de014d8e3aba3ad9c6b7f842d93b68af5d
Darío Javier Cravero <dario@uxtemple.com>
2016-04-25 10:10:25 +0100

    fix: don't trip on non-strings on Text

--- text.js
+++ text.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default ({element: Element='div', lineBreak: marginTop, style, text=''}) => (
   <Element style={style}>
-    {text.split('\n').map((t, i) => <div key={i} style={i ? {marginTop} : {}}>{t}</div>)}
+    {`${text}`.split('\n').map((t, i) => <div key={i} style={i ? {marginTop} : {}}>{t}</div>)}
   </Element>
 );
