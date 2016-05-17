3caa63e201e8f27bf9bf70560f4ddf4558ad214f
Darío Javier Cravero <dario@uxtemple.com>
2016-05-17 14:56:14 +0100

    feat: add Knocking to Image while loading

--- image.js
+++ image.js
@@ -1,27 +1,65 @@
-import React, { PropTypes } from 'react';
+import Knocking from './knocking';
+import React, { Component, PropTypes } from 'react';
 
-const Image = ({_pages={}, text, src, style, styleWrapper}) => (
-  <div style={styleWrapper} {..._pages}>
-    <img
-      alt={text}
-      src={src}
-      style={style}
-      title={text} />
-  </div>
-);
+class Image extends Component {
+  constructor(props) {
+    super(props);
+    this.onLoad = this.onLoad.bind(this);
+    this.state = {
+      isLoading: true
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.src !== nextProps.src) {
+      this.setState({
+        isLoading: true
+      });
+    }
+  }
+
+  onLoad() {
+    this.setState({
+      isLoading: false
+    });
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    const { _pages = {}, text, src, style, styleLoading, styleWrapper } = this.props;
+
+    return (
+      <div style={styleWrapper} {..._pages}>
+        {isLoading && <Knocking style={styleLoading} />}
+        <img
+          alt={text}
+          onLoad={this.onLoad}
+          src={src}
+          style={style}
+          title={text}
+        />
+      </div>
+    );
+  }
+}
 
 Image.defaultProps = {
   src: 'https://files.usepages.today/usepages.today/image-placeholder.svg',
   style: {},
+  styleLoading: {
+    position: 'absolute'
+  },
   styleWrapper: {},
   text: 'Alternative text'
 };
 
-Image.description = `Add some text for when the image can't be displayed.`;
+Image.description = "Add some text for when the image can't be displayed.";
 
 Image.propTypes = {
+  _pages: PropTypes.object,
   src: PropTypes.string.isRequired,
   style: PropTypes.object,
+  styleLoading: PropTypes.object,
   styleWrapper: PropTypes.object,
   text: PropTypes.string
 };
