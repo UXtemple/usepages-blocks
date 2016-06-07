259eb713feebc4d58a7e872d60b4959c325b1a66
Darío Javier Cravero <dario@uxtemple.com>
2016-06-07 22:26:16 +0100

    chore: update List and Unknown

--- unknown.js
+++ unknown.js
@@ -2,16 +2,11 @@ import Code from './code';
 import React from 'react';
 import stringify from 'json-stable-stringify';
 
-export default props => {
-  const { _pages={}, ...rest } = props;
-
-  const code = stringify({
-    block: _pages.block,
-    props: rest
-  }, {space: '\t'});
+export default ({ 'data-block': dataBlock, ...props }) => {
+  const code = stringify(props, {space: '\t'});
 
   return (
-    <div style={style.wrapper} {..._pages}>
+    <div data-block={dataBlock} style={style.wrapper}>
       <div>Sorry but I don't know how to render this block :/</div>
       <Code code={code} gutter={false} readOnly={true} highlightActiveLine={false} style={style.code} />
     </div>
