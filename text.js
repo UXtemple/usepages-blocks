96c668f7d55152d7ed3fe4f5f7e034d4739e5d5d
Darío Javier Cravero <dario@uxtemple.com>
2016-08-04 18:35:12 +0100

    fix(Text): drop element and default to empty string

--- text.js
+++ text.js
@@ -1,27 +1,25 @@
 import React, { PropTypes } from 'react';
 
-const Text = ({ 'data-block': dataBlock, element: Element, lineBreak, style, text }) => {
+const Text = ({ 'data-block': dataBlock, lineBreak, style, text }) => {
   const styleLine = { marginTop: lineBreak };
 
   return (
-    <Element data-block={dataBlock} style={style}>
+    <div data-block={dataBlock} style={style}>
       {`${text}`.split('\n').map((t, i) => (
         <div key={i} style={i ? styleLine : undefined}>{t}</div>
       ))}
-    </Element>
+    </div>
   );
 };
 
 Text.defaultProps = {
-  element: 'div',
   lineBreak: 10,
   style: {},
-  text: 'Write some text'
+  text: ''
 };
 
 Text.propTypes = {
   'data-block': PropTypes.number,
-  element: PropTypes.any,
   lineBreak: PropTypes.number,
   style: PropTypes.object,
   text: PropTypes.string.isRequired
