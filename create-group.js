c7502c71946f0fc18eb61c43666e3ecf59671713
Darío Javier Cravero <dario@uxtemple.com>
2016-10-06 20:40:12 +0100

    feat: allow for styleHover to apply to a Group without a special action

--- create-group.js
+++ create-group.js
@@ -1,54 +1,72 @@
-import { Teleport } from 'panels-ui';
-import GoTo from './go-to';
-import OnClick from './on-click';
-import React, { Component, PropTypes } from 'react';
+import { Teleport } from 'panels-ui'
+import GoTo from './go-to'
+import OnClick from './on-click'
+import React, { Component, PropTypes } from 'react'
+import toCSS from 'style-to-css'
+import uniqueId from 'mini-unique-id'
 
 export default function createGroup(name, groupStyle) {
   class Group extends Component {
-    render() {
-      const { children, goTo, style, teleportTo, ...props } = this.props;
-      const { pages } = this.context;
+    constructor(props, context) {
+      super(props, context)
 
-      let Base;
+      this.className = `Horizontal-${uniqueId()}`
+    }
+
+    render() {
+      const { children, goTo, style, teleportTo, ...props } = this.props
+      const { className } = this
+      const { pages } = this.context
+
+      const finalStyle = {
+        flexWrap: 'wrap',
+        ...groupStyle,
+        ...style
+      }
+
+      let Base
       if (teleportTo) {
-        Base = Teleport;
-        props.to = teleportTo;
+        Base = Teleport
+        props.to = teleportTo
       } else if (goTo) {
-        Base = GoTo;
-        props.href = goTo;
+        Base = GoTo
+        props.href = goTo
       } else if (props.onClick) {
-        Base = OnClick;
+        Base = OnClick
       } else {
-        Base = 'div';
-        if (props._ref) {
-          props.ref = props._ref;
-          delete props._ref;
-        }
-        delete props.styleActive;
-        delete props.styleHover;
+        const { _ref, styleActive, styleHover, ...rest } = props
+
+        const inlineStyle = styleHover ? `.${className}:hover {${toCSS(styleHover)}}` : ''
+
+        return (
+          <div
+            {...rest}
+            className={className}
+            style={finalStyle}
+          >
+            <style>{inlineStyle}</style>
+            {children}
+          </div>
+        )
       }
 
       if (pages && pages.isSelecting) {
         props.onClick = event => {
           if (event) {
-            event.preventDefault();
+            event.preventDefault()
           }
-          return true;
-        };
+          return true
+        }
       }
 
       return (
         <Base
           {...props}
-          style={{
-            flexWrap: 'wrap',
-            ...groupStyle,
-            ...style
-          }}
+          style={finalStyle}
         >
           {children}
         </Base>
-      );
+      )
     }
   }
 
@@ -56,15 +74,15 @@ export default function createGroup(name, groupStyle) {
     pages: PropTypes.shape({
       isSelecting: PropTypes.bool
     })
-  };
+  }
 
   Group.defaultProps = {
     style: {},
     styleActive: {},
     styleHover: {}
-  };
+  }
 
-  Group.displayName = name;
+  Group.displayName = name
 
   Group.propTypes = {
     blocks: PropTypes.any,
@@ -78,7 +96,7 @@ export default function createGroup(name, groupStyle) {
     styleActive: PropTypes.object,
     styleHover: PropTypes.object,
     teleportTo: PropTypes.string
-  };
+  }
 
-  return Group;
+  return Group
 }
